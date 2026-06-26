3891b902cffdc51fcdfd226f6d90a967e6104ebc
youssi12 <y.oulahcene@esi-sba.com>
2026-06-26 14:53:53 +0100

    Remove standalone output for Netlify

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
+ /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
   },
