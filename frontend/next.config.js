e02741345344ba30d0f2a3c7dbe1c0c070323578
youssi12 <y.oulahcene@esi-sba.com>
2026-06-26 14:57:26 +0100

    Configure frontend for Netlify

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
- /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
   },
 };
 
